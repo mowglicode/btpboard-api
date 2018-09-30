457c8b61170c49c06fca22ce2d6042ed469b68bf
Aleck Vincent <aleck.vincent@gmail.com>
2018-09-30 20:30:06 +0200

    added Api Swagger documentation

--- src/main/java/io/btpboard/controller/ProjectController.java
+++ src/main/java/io/btpboard/controller/ProjectController.java
@@ -3,6 +3,7 @@ package io.btpboard.controller;
 import io.btpboard.ProjectDTO;
 import io.btpboard.persistance.entity.Project;
 import io.btpboard.service.IProjectService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,21 +16,25 @@ public class ProjectController {
     @Autowired
     private IProjectService service;
 
+    @ApiOperation(value = "Get all projects")
     @RequestMapping(method = RequestMethod.GET)
     public List<ProjectDTO> findAll() {
         return service.findAll();
     }
 
+    @ApiOperation(value = "Get a project by id")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ProjectDTO findOne(@PathVariable long id) {
         return service.findOne(id);
     }
 
+    @ApiOperation(value = "Save a new project")
     @RequestMapping(method = RequestMethod.POST)
     public ProjectDTO save(@RequestBody Project project) {
         return service.save(project);
     }
 
+    @ApiOperation(value = "Delete a project by id")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable long id) {
         service.delete(id);
