5bb9861f6123dec7d035a638e40aa72af50d9763
Mowgli Code <code.mowgli@gmail.com>
2018-10-08 11:19:28 +0200

    added new fields (description, address, zipcode, city, country)

--- src/main/java/io/btpboard/dto/ProjectDTO.java
+++ src/main/java/io/btpboard/dto/ProjectDTO.java
@@ -4,6 +4,11 @@ public class ProjectDTO {
 
     private long id;
     private String title;
+    private String description;
+    private String address;
+    private String country;
+    private int zipCode;
+    private String city;
 
     public long getId() {
         return id;
@@ -19,5 +24,45 @@ public class ProjectDTO {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 }
